f91545890fc3ce2cbcaa285db66bdcda3275de04
Alissa Wu <alissawu77@gmail.com>
2025-03-16 21:11:05 -0400

    barebones front-end appearance up, textbox is able to take input, started making the Vue app

---
+++ script.js
@@ -0,0 +1,21 @@
+//createApp - function by Vue 3 to create new Vue app instance, app init + mount to DOM elem
+// ref - function by Vue 3 to create a reactive reference to a value. can create reactive state vars to use in Vue components
+const {createApp, ref} = Vue;
+
+const app = createApp({
+    setup() {
+        // reactive data
+        const inputText = ref('');
+        // function to get summary from API
+        const getSummary = () => {
+            console.log('Text to summarize', inputText.value); // health check make sure got the value
+            // call the API
+        }
+        return {
+            inputText, 
+            getSummary
+        }
+    }
+});
+
+app.mount('#app'); // mount the app to the DOM element with id 'app'
