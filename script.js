d0b8cdc279e89c48e136bcc30f64b9b9d9a2e6f7
Alissa Wu <alissawu77@gmail.com>
2025-03-16 22:06:16 -0400

    Summary msgs show up, simulated errors, values, etc all work

--- script.js
+++ script.js
@@ -4,16 +4,51 @@ const {createApp, ref} = Vue;
 
 const app = createApp({
     setup() {
-        // reactive data
+        // CREATE REACTIVE STATE VARS
         const inputText = ref('');
+        const summary = ref('');
+        const error = ref(''); 
+        const isLoading = ref(false); 
+
         // function to get summary from API
-        const getSummary = () => {
-            console.log('Text to summarize', inputText.value); // health check make sure got the value
-            // call the API
+        const getSummary = async () => {
+            // NO TEXT ERROR
+            if(!inputText.value.trim()){
+                error.value = 'Please enter text to summarize';
+                return;
+            }
+            try {
+                // reactive state variable, value is stored in .value
+                isLoading.value = true;
+                error.value = '';
+                // async returns a Promise (object), await pauses the function until the Promise is resolved or rejected
+                // Promise - object that represents eventual completion or failure of async operation, and its result value
+                // has 3 states - Pending, Fufilled (Resolved), Rejected
+                // resolve - fn to call when succeed, reject - fn to call when fail
+                await new Promise((resolve, _) => setTimeout(resolve, 1000)); // resolve is the callback fn called after the delay
+                summary.value = inputText.value.trim();
+                // simulate error
+                if(inputText.value.trim().length > 10){
+                    throw new Error('Text too long');
+                }
+            }
+            catch (err) {
+                summary.value = '';
+                /* have default error message */
+                error.value = err ? err.message : 'An error occurred';
+            }
+            finally {
+                isLoading.value = false;
+            }
+            
+
         };
         return {
             inputText, 
-            getSummary
+            getSummary,
+            summary,
+            error,
+            isLoading
         };
     }
 });
